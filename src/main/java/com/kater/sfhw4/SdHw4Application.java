6587376c8a442fb10919c277b29d1dca6baf5849
Evgeny <startsev2000@outlook.com>
2023-05-21 16:31:34 +0300

    First commit

---
+++ src/main/java/com/kater/sfhw4/SdHw4Application.java
@@ -0,0 +1,13 @@
+package com.kater.sfhw4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdHw4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdHw4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kater/sfhw4/SdHw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kater.sfhw4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdHw4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
